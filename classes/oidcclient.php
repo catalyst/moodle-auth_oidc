bff9c59ef7fc5581a0231b0e7ef24c67c6b66d36
Peter Burnett <peterburnett@catalyst-au.net>
2023-03-07 11:27:49 +1000

    Bugfix

--- classes/oidcclient.php
+++ classes/oidcclient.php
@@ -341,7 +341,7 @@ class oidcclient {
                 $params['tenant'] = 'common';
                 break;
             default:
-                $params = $sendsecret ? ['client_secret' => $this->clientsecret] : [];
+                $params = $params['client_secret'] = $sendsecret ? $this->clientsecret : null;
         }
         $returned = $this->httpclient->post($this->endpoints['token'], $params);
         return utils::process_json_response($returned, ['id_token' => null]);
