1219644db640d234b3d9e65a78b23315e28fffd0
chenli <chenli@your.hostname.com>
2025-04-03 14:40:23 +1100

    Remove whitespace for testing

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -80,7 +80,7 @@ class provider implements
                 'userid',
                 'sid',
                 'timecreated',
-            ],            
+            ],
         ];
 
         foreach ($tables as $table => $fields) {
@@ -205,7 +205,7 @@ class provider implements
         $tables = [
             'auth_oidc_prevlogin' => ['userid' => $user->id],
             'auth_oidc_token' => ['userid' => $user->id],
-            'auth_oidc_sid' => ['userid' => $user->id],            
+            'auth_oidc_sid' => ['userid' => $user->id],       
         ];
         return $tables;
     }
